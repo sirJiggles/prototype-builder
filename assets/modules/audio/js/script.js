97ccf59ee3aed0308f3648144656ad52e7894104
Gareth Fuller <gareth@honestideas.co.uk>
2013-04-04 08:37:35 +0100

    adding first version of audio module to the builder GF

---
+++ assets/modules/audio/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    
+    $('audio').mediaelementplayer({
+        flashName: '/flash/flashmediaelement.swf',
+        silverlightName: '/flash/silverlightmediaelement.xap',
+        features: ['playpause', 'progress', 'current','duration']
+
+    });
+    // HTML FALLBACK
+    $('.me-cannotplay').parent().parent().parent().parent().find('.media-controls').css('display','block');
+    $('.me-cannotplay').parent().parent().parent().css('display', 'none');
+            
+});
