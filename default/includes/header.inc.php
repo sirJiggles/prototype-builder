ee8b25bc55b57dcddc596d1c41f5cb25d47fd79a
Gareth Fuller <gareth@honestideas.co.uk>
2013-04-23 16:57:42 +0100

    more little tweaks to modules GF

--- default/includes/header.inc.php
+++ default/includes/header.inc.php
@@ -11,7 +11,7 @@
         <!-- META -->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1, minimum-scale=1" />
     	<meta name="description" content="" />
     	<meta name="author" content="" />
         <meta name="keywords" content="" />
--- includes/header.inc.php
+++ includes/header.inc.php
@@ -11,8 +11,8 @@
         <!-- META -->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    	<meta name="description" content="" />
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1, minimum-scale=1" />
+        <meta name="description" content="" />
     	<meta name="author" content="" />
         <meta name="keywords" content="" />
 	
